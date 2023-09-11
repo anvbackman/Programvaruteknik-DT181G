3ee5c815195c63c375e5fca7f443b530538c6f91
Andreas Backman <anba2205@student.miun.se>
2023-09-11 17:22:43 +0200

    Comments

--- Laboration_1/src/main/java/com/dt181g/laboration_1/PoolManager.java
+++ Laboration_1/src/main/java/com/dt181g/laboration_1/PoolManager.java
@@ -4,17 +4,22 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 /**
- * PoolManager class that mmanages a pool of resources
+ * PoolManager class implemented as an enum Singleton that manages a pool of resources
+ * which provides methods to acquire and return resources in a thread-safe manner.
  * @author Andreas Backman
  */
-public class PoolManager {
+public enum PoolManager {
 
-    private static final PoolManager poolManager = new PoolManager();
+    INSTANCE;
 
     private static final int POOL_SIZE = 5;
 
     private final Queue<Resource> pool;
 
+    /**
+     * Private constructor that initializes the resource pool
+     * and then creates resources that gets added to the pool.
+     */
     private PoolManager() {
         pool = new LinkedList<>();
         for (int i = 1; i <= POOL_SIZE; i++) {
@@ -22,25 +27,38 @@ public class PoolManager {
         }
     }
 
+    /**
+     * Method used to get resources from the pool.
+     *
+     * @return a resource from the pool
+     * @throws InterruptedException if the thread is interrupted while its waiting for a resource
+     */
     public static Resource getResource() throws InterruptedException {
+        // Prints a message to tell when a thread is ready to be used
         System.out.println(Thread.currentThread().getName() + " is ready to be used");
-        synchronized (poolManager) {
-            while(poolManager.pool.isEmpty()) {
-                poolManager.wait();
+        synchronized (INSTANCE.pool) {
+            // Loop to wait until a resource is available
+            while(INSTANCE.pool.isEmpty()) {
+                INSTANCE.pool.wait();
             }
-            Resource resource = poolManager.pool.poll();
-            resource.setStartTimer();
+            Resource resource = INSTANCE.pool.poll(); // Get a resource from the pool
+            resource.setStartTimer(); // Starts a timer for when the resource is received
             return resource;
         }
     }
 
+    /**
+     * Method used to return a resource back to the pool
+     * @param resource the resource to be returned
+     */
     public static void returnResource(Resource resource) {
-        resource.setEndTimer();
+        resource.setEndTimer(); // Sets the timer for when a resource is returned
+        // Prints a message for the return
         System.out.println("Resource with ID: " + resource.getId() + ", was returned from: " + Thread.currentThread().getName()
                 + ", after being held for " + resource.getTimer() + " milliseconds");
-        synchronized (poolManager.pool) {
-            poolManager.pool.offer(resource);
-            poolManager.pool.notifyAll();
+        synchronized (INSTANCE.pool) {
+            INSTANCE.pool.offer(resource); // Adds resource back to the pool
+            INSTANCE.pool.notifyAll(); // Notifies all waiting threads that a resource is available
         }
     }
 }
--- Laboration_1/src/main/java/com/dt181g/laboration_1/Resource.java
+++ Laboration_1/src/main/java/com/dt181g/laboration_1/Resource.java
@@ -1,28 +1,54 @@
 package com.dt181g.laboration_1;
 
+/**
+ * Resource class that is used to represent a resources ID and to set a time intervall for how long
+ * a thread has been used by the resource.
+ *
+ * @author Andreas Backman
+ */
 public class Resource {
 
     private final int id;
     private long startTimer;
     private long endTimer;
 
+    /**
+     * Creating a resource with the specified ID.
+     *
+     * @param id the resource id
+     */
     public Resource(int id) {
         System.out.println("Resource with ID: " + id + ", was created");
         this.id = id;
     }
 
+    /**
+     * Gets the ID of the resource
+     *
+     * @return the ID of the resource
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Method to set starting time
+     */
     public void setStartTimer() {
         startTimer = System.currentTimeMillis();
     }
 
+    /**
+     * Method to set ending time
+     */
     public void setEndTimer() {
         endTimer = System.currentTimeMillis();
     }
 
+    /**
+     * Method for calculating how long the thread has held a resource
+     * @return the calculated time
+     */
     public long getTimer() {
         return endTimer - startTimer;
     }
