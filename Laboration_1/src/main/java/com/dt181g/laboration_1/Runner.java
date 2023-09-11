f1384a3296f8ef3953fcf6b07ed01056147a8cc2
Andreas Backman <anba2205@student.miun.se>
2023-09-11 18:26:10 +0200

    Final commit

--- Laboration_1/src/main/java/com/dt181g/laboration_1/Runner.java
+++ Laboration_1/src/main/java/com/dt181g/laboration_1/Runner.java
@@ -1,21 +1,39 @@
 package com.dt181g.laboration_1;
 
+/**
+ * Runner class that implements Runnable interface that acquires resources from the pool
+ * uses them and then returns the resource back into the pool.
+ *
+ *
+ * @author Andreas Backman
+ */
 public class Runner implements Runnable  {
     private final int id;
+
+    /**
+     * Creates a new Runner with the provided id
+     *
+     * @param id the id for the runner
+     */
     public Runner(int id) {
         this.id = id;
     }
 
+    /**
+     * Method to execute the runner thread by running through a loop that acquires resources, use them and then
+     * return the resource back into the pool while also handeling interruptions
+     */
     @Override
     public void run() {
         while (!Thread.currentThread().isInterrupted()) {
             try {
-                Resource resource =  PoolManager.getResource();
+                Resource resource =  PoolManager.getResource(); // Acquire a resource from the pool
+                // Prints a message using the resource id and thread name
                 System.out.println("Resource with ID: " + resource.getId() + ", was received by: " + Thread.currentThread().getName());
-                Thread.sleep(1000);
-                PoolManager.returnResource(resource);
+                Thread.sleep(1000); // Making the thread sleep
+                PoolManager.returnResource(resource); // Returns the resource to the pool
             }
-            catch (InterruptedException e) {
+            catch (InterruptedException e) { // Throws InterruptedException if current thread is interrupted
                 Thread.currentThread().interrupt();
                 System.out.println("Thread interrupted");
             }
