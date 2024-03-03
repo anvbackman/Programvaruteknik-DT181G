b33fda65fd4fa83fef090cdcafe4999b91af32ba
Andreas Backman <anba2205@student.miun.se>
2024-03-03 18:48:45 +0100

    Final touch

--- Project/src/main/java/com/dt181g/project/views/ButtonPanel.java
+++ Project/src/main/java/com/dt181g/project/views/ButtonPanel.java
@@ -20,16 +20,19 @@ public class ButtonPanel extends JPanel {
     public ButtonPanel(ActionListener infoActionListener, ActionListener quitActionListener) {
         // Sets button layout
         setLayout(new FlowLayout());
+        Dimension size = new Dimension(200, 50);
 
         // Creates buttons for info and quitting, adds it to the panel, setFocusable to not be focused by space bar
         // Then adds the ActionListener to the button
         infoButton = new JButton("Game Info");
         add(infoButton);
+        infoButton.setPreferredSize(size);
         infoButton.setFocusable(false);
         infoButton.addActionListener(infoActionListener);
 
         quitButton = new JButton("Quit");
         add(quitButton);
+        quitButton.setPreferredSize(size);
         quitButton.setFocusable(false);
         quitButton.addActionListener(quitActionListener);
     }
